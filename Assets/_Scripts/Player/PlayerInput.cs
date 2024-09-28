c944c6de7048eedbbacc250ea360fc2382e8992a
RabsuiraGH <rabsuira@proton.me>
2024-09-28 10:50:39 +0300

    Added possibility to hold movement button

--- Assets/_Scripts/Player/PlayerInput.cs
+++ Assets/_Scripts/Player/PlayerInput.cs
@@ -1,3 +1,5 @@
+using System.Threading;
+using System.Threading.Tasks;
 using Core.Input;
 using Core.Player.Movement;
 using UnityEngine;
@@ -8,9 +10,15 @@ namespace Core.Player.Input
 {
     public class PlayerInput : MonoBehaviour
     {
-        [SerializeField] private BaseInput _baseInput;
+        [SerializeField] private BaseInput _baseInput = null;
 
-        [SerializeField] private IMoveable _player;
+        [SerializeField] private IMoveable _player = null;
+
+        [SerializeField] private bool _isHoldingMovement = false;
+
+        [SerializeField] private float _repeatRate = 0.75f;
+
+        private CancellationTokenSource _cancellationTokenSource = null;
 
         [Inject]
         public void Construct(IMoveable player, BaseInput baseInput)
@@ -21,11 +29,41 @@ namespace Core.Player.Input
 
         private void Awake()
         {
-            _baseInput.Gameplay.Movement.started += PerformMovement;
+            _baseInput.Gameplay.Movement.started += OnMovementStarted;
+            _baseInput.Gameplay.Movement.canceled += OnMovementCanceled;
+        }
+
+        private void OnMovementStarted(InputAction.CallbackContext context)
+        {
+            if (_isHoldingMovement)
+                return;
+
+            _cancellationTokenSource = new CancellationTokenSource();
+
+            _isHoldingMovement = true;
+            HoldMovementAsync(context, _cancellationTokenSource); // Запуск асинхронного метода
+        }
+
+        private void OnMovementCanceled(InputAction.CallbackContext context)
+        {
+            _isHoldingMovement = false;
+            _cancellationTokenSource.Cancel();
+        }
+
+        private async void HoldMovementAsync(InputAction.CallbackContext context, CancellationTokenSource token)
+        {
+            while (_isHoldingMovement)
+            {
+                if (token.IsCancellationRequested)
+                    return;
+
+                PerformMovement(context);
+
+                await Task.Delay((int)(_repeatRate * 1000));
+            }
         }
 
         private void PerformMovement(InputAction.CallbackContext context)
-
         {
             Vector2 direction = GetProperlyDirection(context.ReadValue<Vector2>());
 
@@ -48,7 +86,11 @@ namespace Core.Player.Input
 
         private void OnDestroy()
         {
-            _baseInput.Gameplay.Movement.started -= PerformMovement;
+            _cancellationTokenSource.Cancel();
+            _cancellationTokenSource.Dispose();
+
+            _baseInput.Gameplay.Movement.started -= OnMovementStarted;
+            _baseInput.Gameplay.Movement.canceled -= OnMovementCanceled;
         }
     }
 }
